e34a5eeb5b67cf3f40dae88c00264ef2175f9a1b
y4shiro <y4shironao@gmail.com>
2022-01-11 21:59:33 +0900

    入力フォームの高さ自動調節機能をカスタムフックとして切り出し

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -24,16 +24,7 @@ export const InputForm: React.VFC<Props> = ({
     onConfirm?.();
   };
 
-  const ref = useRef<HTMLTextAreaElement>(null);
-
-  useEffect(() => {
-    const el = ref.current;
-    if (!el) return;
-
-    const { borderTopWidth, borderBottomWidth } = getComputedStyle(el);
-    el.style.height = 'auto'; // 一度 auto に設定しないと高さが縮まなくなる
-    el.style.height = `calc(${borderTopWidth} + ${el.scrollHeight}px + ${borderBottomWidth})`;
-  }, [value]);
+  const ref = useAutoFitToContentHeight(value);
 
   return (
     <Container className={className}>
@@ -91,3 +82,19 @@ const AddButton = styled(ConfirmButton).attrs({
 const CancelButton = styled(Button).attrs({
   children: 'Cancel',
 })``;
+
+// テキストエリアの高さを内容に応じて自動調整する
+const useAutoFitToContentHeight = (content: string | undefined) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    const el = ref.current;
+    if (!el) return;
+
+    const { borderTopWidth, borderBottomWidth } = getComputedStyle(el);
+    el.style.height = 'auto'; // 一度 auto に設定しないと高さが縮まなくなる
+    el.style.height = `calc(${borderTopWidth} + ${el.scrollHeight}px + ${borderBottomWidth})`;
+  }, [content]);
+
+  return ref;
+};
