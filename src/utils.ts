de2a02589a78eee194e9ccae1af8b4003c763216
y4shiro <y4shironao@gmail.com>
2022-01-13 11:58:17 +0900

    cardsOrder を API で取得して、カードを column ごとに順列通りに表示

--- src/utils.ts
+++ src/utils.ts
@@ -10,3 +10,29 @@ export const randomID = () => {
 
   return id;
 };
+
+// 受け取ったリストをリスト順序情報に従ってソートし、新たなリストを返す
+export const sortBy = <
+  E extends { id: Exclude<V, null> },
+  V extends string | null,
+>(
+  list: E[],
+  order: Record<string, V>,
+  head: Exclude<V, null>,
+) => {
+  const map = list.reduce((m, e) => m.set(e.id, e), new Map<V, E>());
+
+  const sorted: typeof list = [];
+
+  let id = order[head];
+  for (let i = list.length; i > 0; i--) {
+    if (!id || id === head) break;
+
+    const e = map.get(id);
+    if (e) sorted.push(e);
+
+    id = order[id as Exclude<V, null>];
+  }
+
+  return sorted;
+};
