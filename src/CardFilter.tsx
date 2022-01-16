7f689bae7ee6471ac25b9852c22ddc8282418ff7
y4shiro <y4shironao@gmail.com>
2022-01-16 17:47:51 +0900

    CardFilter への入力を debounce して不要な再レンダリングを抑制

--- src/CardFilter.tsx
+++ src/CardFilter.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useStore } from 'react-redux';
 import * as color from './color';
 import { SearchIcon as _SearchIcon } from './icon';
 
 export const CardFilter: React.VFC = () => {
+  const store = useStore();
   const dispatch = useDispatch();
-  const value = useSelector(state => state.filterValue);
-  const onChange = (value: string) =>
-    dispatch({
-      type: 'Filter.SetFilter',
-      payload: {
-        value,
-      },
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    store.subscribe(() => {
+      const { filterValue } = store.getState();
+      if (value === filterValue) return;
+      setValue(filterValue);
     });
+  }, [store, value]);
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => dispatch({ type: 'Filter.SetFilter', payload: { value } }),
+      400,
+    );
+
+    return () => clearTimeout(timer);
+  }, [dispatch, value]);
 
   return (
     <Container>
@@ -21,7 +32,7 @@ export const CardFilter: React.VFC = () => {
       <Input
         placeholder="Filter cards"
         value={value}
-        onChange={ev => onChange(ev.currentTarget.value)}
+        onChange={ev => setValue(ev.currentTarget.value)}
       />
     </Container>
   );
