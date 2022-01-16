a2348954945ff7c25440c2fb0f7ccf8241d58913
y4shiro <y4shironao@gmail.com>
2022-01-16 15:26:48 +0900

    InputForm の余計な import を削除

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { randomID, reorderPatch } from './utils';
